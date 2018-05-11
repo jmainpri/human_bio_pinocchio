dc06c9f7b6f7d825a4d67b6fd4c1fd47c9f08425
Jim <mainprice@gmail.com>
2018-05-11 14:54:59 +0200

    Remove reference to ROS

--- src/sample_random_config.py
+++ src/sample_random_config.py
@@ -21,12 +21,10 @@ from pinocchio.utils import *
 from pinocchio.explog import exp,log
 from numpy.linalg import pinv,norm
 import pinocchio as se3
-import rospkg
+import utils
 
 print se3
-rospack = rospkg.RosPack()
-human = se3.RobotWrapper(
-    str(rospack.get_path('human_bio_urdf') + "/urdf/human_bio.urdf"))
+human = se3.RobotWrapper(utils.human_urdf_path())
 q = human.q0
 print "shape of q : ", q.shape
 print "human : ", human
