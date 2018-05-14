Fix jacobian in global frame

src/iterative_ik.py
@@ -35,25 +35,30 @@ class IterativeIK:
         function as well as the Jacobian.
     """
     def __init__(self, 
-        jacobian_fct, 
-        forward_kinematics, 
+        jacobian_fct,
+        forward_kinematics_fct, 
         active_dofs=None,
         lower_limits=[],
         upper_limits=[]):
+
         self.verbose = True
         self.q_full = None
-        self.__check_joint_limits = False
+        self.configurations = []
+        
         self.__jacobian_fct = jacobian_fct
-        self.__forward_kinematics_fct = forward_kinematics
+        self.__forward_kinematics_fct = forward_kinematics_fct
         self.__active_dofs = active_dofs
         self.__lower_limits = lower_limits
         self.__upper_limits = upper_limits
-        self.__limits = None
+        
+        self.__check_joint_limits = (
+            len(self.__lower_limits)>0 and 
+            len(self.__upper_limits)>0 )
+
         self.__sigmoid_joint_limits = None
-        self.__magnitude = 0.1
-        self.__max_nb_iterations = 100
-        self.__max_distance_to_target = 0.01
-        self.__configurations = []
+        self.__magnitude = 0.002
+        self.__max_nb_iterations = 1000
+        self.__max_distance_to_target = 0.005
         self.__conservative_joint_limit_threshold = 1e-5
         
 
@@ -98,25 +103,26 @@ class IterativeIK:
         for active dofs and match in size the lower and upper argument of 
         of the class. A bad joint indicies list is mantained
         """
-        assert q.shape[0] == self.__active_dofs
-        assert q.shape[0] == self.__lower_limits_.shape[0] 
-        assert q.shape[0] == self.__upper_limits_.shape[0]
+        assert q.shape[0] == len(self.__active_dofs)
+        assert q.shape[0] == len(self.__lower_limits)
+        assert q.shape[0] == len(self.__upper_limits)
         violate_limit = False
-        theshold = self.__conservative_joint_limit_threshold_
-        for idx in range(self.__active_dofs_):
-            if(q[i] < (self.__lower_limits_[idx] + theshold) or 
-                q[i] > (self.__upper_limits_[idx] - theshold)):
+        theshold = self.__conservative_joint_limit_threshold
+        bad_joint_indices = []
+        for idx in range(len(self.__active_dofs)):
+            if( q[idx] < (self.__lower_limits[idx] + theshold) or 
+                q[idx] > (self.__upper_limits[idx] - theshold)):
                 # note this will never add the same joint
                 # twice, even if bClearBadJoints = false
-                bad_joint_indices.append(i)
+                bad_joint_indices.append(idx)
                 violate_limit = True
 
-                if self.__verbose_: 
+                if self.verbose: 
                     # cout << "does not respect joint limits : "
                     #     << active_joints_[i]->getName();
-                    print "ith joint : ", i
-                    print " , lower limit : ", self.__lower_limits_[idx]
-                    print " , upper limit : ", self.__upper_limits_[idx]
+                    print "ith joint : ", idx
+                    print " , lower limit : ", self.__lower_limits[idx]
+                    print " , upper limit : ", self.__upper_limits[idx]
                     print " , q_j : ", q[idx] 
 
         return violate_limit
@@ -137,7 +143,7 @@ class IterativeIK:
         in active_indices_
         """
         J = self.jacobian(q)
-        print "J.shape : ", J.shape
+        # print "J : ", J
         assert q.shape[0] == len(self.__active_dofs)
         assert x_des.shape[0] == x_pose.shape[0]  # size of task space
         assert x_des.shape[0] == J.shape[0]       # size of task space
@@ -150,42 +156,46 @@ class IterativeIK:
         dq = J_plus * self.__magnitude * normalize(x_des - x_pose)
         return dq
 
-
     def solve(self, q, x_des):
         """
         Solve for a full inverse kinematics pose
         the function saves the intermediate configuration
         to generate a trajectory.
+        Assumes q is given for active dofs, all configurations
+        in this function are only assumed for active dofs.
         """
         if self.__check_joint_limits:
             violates_limits = self.check_violate_joint_limits(q)
             if violates_limits:
                 print "Warning configuration violates joint limits"
 
-        print "x_des : ", x_des.transpose()
-
         has_succeeded = False
-        self.__configurations = []
+        self.configurations = []
         q_tmp = np.copy(q)
         x_pose = self.forward_kinematics(q_tmp)
-        print "x_pose : ", x_pose.transpose()
+        print " - Init  dist : ", norm(x_des - x_pose)
         for it in range(self.__max_nb_iterations):
             q_tmp += self.single_step(q_tmp, x_des, x_pose)
-            self.__configurations.append(q_tmp)
+            self.configurations.append(q_tmp)
             x_pose = self.forward_kinematics(q_tmp)
             dist = norm(x_des - x_pose)
             if dist < self.__max_distance_to_target:
                 has_succeeded = True
                 break
+            if self.verbose and it % 100 == 0:
+                print "dist : ", dist
+        print " - final dist : ", dist
         if not has_succeeded:
             print "Ik could not converge in given number of iterations"
-        if has_succeeded and self.__check_joint_limits:
-            violates_limits = self.check_violate_joint_limits(q_tmp)
-            if violates_limits:
-                has_succeeded = False
-                print "Ik succeeded but violates joint limits"
+        else:
+            print "succeeded (iter : {})".format(it)
+            if self.__check_joint_limits:
+                violates_limits = self.check_violate_joint_limits(q_tmp)
+                if violates_limits:
+                    # has_succeeded = False
+                    print "Ik succeeded but violates joint limits"
         return has_succeeded
-        
+
 
 
 

src/test_iterative_ik.py
@@ -23,64 +23,105 @@ import pinocchio as se3
 from pinocchio.utils import *
 from iterative_ik import *
 
-def initialize():
-    # Load config files
-    human=se3.RobotWrapper(utils.human_urdf_path())
-    data=utils.human_config_data()
-    
-    # Setup default translations
-    q_default=np.zeros(human.q0.shape)
-    for name, value in data['right_arm_default'].iteritems():
-        q_default[human.index(name)] = value
-    human.q0 = q_default
+class TestIterativeIk:
 
-    # Setup the active dofs indices
-    active_dofs=[human.index(name) for name in data["right_arm_dofs"]]
-    return human, active_dofs
+    def __init__(self, urdf_path):
+        # Load config files
+        #  utils.human_urdf_path()
+        self.human=se3.RobotWrapper(urdf_path)
+        data=utils.human_config_data()
+        
+        # Setup default translations
+        q_default=np.zeros(self.human.q0.shape)
+        for name, value in data['right_arm_default'].iteritems():
+            q_default[self.human.index(name)] = value
+        self.human.q0 = q_default
 
-def forward_kinematics(q_all, human, wrist_index):
-    human.forwardKinematics(q_all)
-    return human.data.oMi[wrist_index].translation
+        # active dofs indices
+        self.active_dofs=[
+            self.human.index(name) for name in data["right_arm_dofs"]]
 
-def sample_configs(human, active_dofs, wrist_name):
-    print "shape of q : ", human.q0.shape
-    print "human : ", human
-    wrist_index = human.index(wrist_name)
-    nb_configs = 20
-    data = [None] * nb_configs
-    for i in range(nb_configs):
-        q_active=rand((len(active_dofs)))
-        q_all=np.copy(human.q0)
-        q_all[active_dofs] = q_active
-        p = forward_kinematics(q_all, human, wrist_index)
-        data[i] = (q_all, p)
-        print "p_{} : {}".format(i, p.transpose())
-    return data
+        # dof limits
+        model = self.human.model
+        self.lower_limits = model.lowerPositionLimit[self.active_dofs]
+        self.upper_limits = model.upperPositionLimit[self.active_dofs]
+        assert len(self.lower_limits) == len(self.upper_limits)
 
-def test_iterative_ik(human, active_dofs, wrist_name, data):
-    wrist_index = human.index(wrist_name)
-    print "active dofs : ", active_dofs
-    print "q_0.shape : ", human.q0.shape
-    print "wrist_index : ", wrist_index
+        # wrist index
+        wrist_name = "rWristY"
+        self.wrist_index = self.human.index(wrist_name)
 
-    iterative_ik=IterativeIK(
-        lambda q: human.jacobian(q, wrist_index),
-        lambda q: forward_kinematics(q, human, wrist_index),
-        active_dofs)
-    iterative_ik.q_full = np.copy(human.q0)
-    for config in data:
-        succeeded = iterative_ik.solve(
-            config[0][active_dofs], 
-            config[1])
-        if not succeeded:
-            return False
-    return True
+    def forward_kinematics(self, q_all):
+        self.human.forwardKinematics(q_all)
+        return self.human.data.oMi[self.wrist_index].translation
+
+    def sample_q(self):
+        nb_active_dofs = len(self.lower_limits)
+        q_rand = np.random.uniform(self.lower_limits, self.upper_limits)
+        assert nb_active_dofs == q_rand.shape[0]
+        for i in range(q_rand.shape[0]):
+            assert q_rand[i] > self.lower_limits[i]
+            assert q_rand[i] < self.upper_limits[i]
+        return q_rand
+
+    def sample_q_normal(self, q_mean):
+        nb_active_dofs = len(self.lower_limits)
+        q_rand = np.zeros(q_mean.shape)
+        for i in range(q_mean.shape[0]):
+            q_rand[i] = np.random.normal(q_mean[i], 
+                (self.upper_limits[i] - self.lower_limits[i]) / 10. )
+        q_rand = np.clip(q_rand, self.lower_limits, self.upper_limits)
+        assert q_mean.shape == q_rand.shape
+        assert nb_active_dofs == q_rand.shape[0]
+        for i in range(q_rand.shape[0]):
+            assert q_rand[i] >= self.lower_limits[i]
+            assert q_rand[i] <= self.upper_limits[i]
+        return q_rand
+
+    def sample_configs(self):
+        print "shape of q : ", self.human.q0.shape
+        print "human : ", self.human
+        nb_configs = 20
+        data = [None] * nb_configs
+        for i in range(nb_configs):
+            # q_active=rand((len(active_dofs)))
+            q_active=self.sample_q()
+            q_all=np.copy(self.human.q0)
+            q_all[self.active_dofs] = q_active
+            p = self.forward_kinematics(q_all)
+            data[i] = (q_all, p)
+            print "p_{} : {}".format(i, p.transpose())
+        return data
+
+    def run(self, data):
+        print "active dofs : ", self.active_dofs
+        print "q_0.shape : ", self.human.q0.shape
+        print "wrist_index : ", self.wrist_index
+
+        iterative_ik=IterativeIK(
+            lambda q: self.human.jacobian(q, self.wrist_index, True, False),
+            lambda q: self.forward_kinematics(q),
+            self.active_dofs,
+            self.lower_limits,
+            self.upper_limits)
+        iterative_ik.verbose = False
+        iterative_ik.q_full = np.copy(self.human.q0)
+        nb_succeed=0
+        for i, config in enumerate(data):
+            print '******* --- IK nb. {} --- *******'.format(i)
+            succeeded = iterative_ik.solve(
+                self.sample_q_normal(config[0][self.active_dofs]), config[1])
+            if succeeded:
+                nb_succeed += 1
+        print "Total nb if success : {} over {}".format(
+            nb_succeed, len(data))
+        return True
 
 
 if __name__== "__main__":
-    wrist_name = "rWristY"
-    human, active_dofs = initialize()
-    data = sample_configs(human, active_dofs, wrist_name)
-    success = test_iterative_ik(human, active_dofs, wrist_name, data)
+    
+    test_iterative_ik = TestIterativeIk(utils.human_urdf_path())
+    data = test_iterative_ik.sample_configs()
+    success = test_iterative_ik.run(data)
     print  "success : ", success
 