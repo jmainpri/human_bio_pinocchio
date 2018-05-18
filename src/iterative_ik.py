Fix jacobian

src/iterative_ik.py
@@ -42,7 +42,7 @@ class IterativeIK:
         upper_limits=[]):
 
         self.verbose = True
-        self.debug = True
+        self.debug = False
         self.q_full = None
         self.configurations = []
         
@@ -162,10 +162,13 @@ class IterativeIK:
         # J = np.eye(J.shape[0], J.shape[1])
         J_plus = np.matrix(pinv(J[0:3,:], rcond=1e-8))
         # J_plus = np.matrix(J).transpose()
-        # print "J"
-        # print J
-        # print "J_plus"
-        # print J_plus
+        if self.debug:
+            print "J"
+            print J
+            print "J_plus"
+            print J_plus
+            print "dist"
+            print dist
 
         # Warning the way we compute the difference in angle
         # should be worked out to handle task space distances
@@ -199,7 +202,7 @@ class IterativeIK:
         q_tmp = np.copy(q)
         x_pose = self.forward_kinematics(q_tmp)
         dist = self.__dist_fct(x_des, x_pose)
-        print " - Init  dist : ", dist.transpose()
+        print " - Init  dist : ", norm(dist)
         for it in range(self.__max_nb_iterations):
             q_tmp += self.single_step(q_tmp, dist)
             x_pose = self.forward_kinematics(q_tmp)
@@ -207,11 +210,11 @@ class IterativeIK:
             if norm(dist) < self.__max_distance_to_target:
                 has_succeeded = True
             if self.verbose and it % 100 == 0:
-                print "dist : ", dist.transpose()
+                print "dist : ", norm(dist)
             self.configurations.append(self.full_dof_config(q_tmp))
-            if has_succeeded or (it == 10 and self.debug):
+            if has_succeeded:
                 break
-        print " - final dist : ", dist.transpose()
+        print " - final dist : ", norm(dist)
         if not has_succeeded:
             print "Ik could not converge in given number of iterations"
         else:

src/pinocchio_ik.py
@@ -54,12 +54,11 @@ class PinocchioIterativeIk:
         # store trajectories
         self.trajectories = []
 
+    def index_config(self, name):
+        return self.robot.model.getJointId(name) - 1
+
     def index_joint(self, name):
         return self.robot.model.getJointId(name)
-
-    def forward_kinematics(self, q_all):
-        self.robot.forwardKinematics(q_all)
-        return self.robot.data.oMi[self.wrist_index].copy()
 
     def sample_q(self):
         nb_active_dofs = len(self.lower_limits)
@@ -99,20 +98,43 @@ class PinocchioIterativeIk:
             print "p_{} : {}".format(i, se3.translation.transpose())
         return np.array(data)
 
+    def dist(self, x_des, x_pose):
+        # print "x_des ", x_des
+        # print "x_pos ", x_pose
+        # dist = log(x_pose.actInv(x_des)).vector
+        # print "dist"
+        # print dist
+        dist = zero(6)
+        dist[:3] = x_des.translation - x_pose.translation
+        return dist
+
+    def jacobian(self, q):
+        J = self.robot.jacobian(
+                q, 
+                self.wrist_index, 
+                update_kinematics=True, 
+                local_frame=True)
+        # return J
+        o_M_wrist = se3.SE3(
+            self.robot.data.oMi[self.wrist_index].rotation, zero(3))
+        return o_M_wrist.action * J
+
+    def forward_kinematics(self, q_all):
+        self.robot.forwardKinematics(q_all)
+        return self.robot.data.oMi[self.wrist_index].copy()
+
+
     def run(self, data):
         print "active dofs : ", self.active_dofs
         print "q_0.shape : ", self.robot.q0.shape
         print "wrist_index : ", self.wrist_index
         iterative_ik=IterativeIK(
-            lambda x_des, x_pose: log(x_des.act(x_pose.inverse())).vector,
-            lambda q: self.robot.jacobian(
-                q, self.wrist_index, 
-                update_kinematics=True, 
-                local_frame=False),
-            lambda q: self.forward_kinematics(q),
-            self.active_dofs,
-            self.lower_limits,
-            self.upper_limits)
+                self.dist,
+                self.jacobian,
+                self.forward_kinematics,
+                self.active_dofs,
+                self.lower_limits,
+                self.upper_limits)
         iterative_ik.verbose = False
         iterative_ik.q_full = np.copy(self.robot.q0)
         nb_succeed=0
@@ -120,9 +142,10 @@ class PinocchioIterativeIk:
             print '******* --- IK nb. {} --- *******'.format(i)
             iterative_ik.configurations = []
             succeeded = iterative_ik.solve(
-                self.sample_q_normal(config[0][self.active_dofs]),
-                # self.robot.q0[self.active_dofs],
-                config[1])
+                # self.sample_q_normal(config[0][self.active_dofs]),
+                self.robot.q0[self.active_dofs],
+                config[1]
+                )
             self.trajectories.append(np.stack(iterative_ik.configurations))
             if succeeded:
                 nb_succeed += 1

src/test_jacobian_plannar.py
@@ -42,15 +42,14 @@ if __name__== "__main__":
     test_iterative_ik = TestPlanarIterativeIk()
     data = test_iterative_ik.sample_configs()
     iterative_ik=IterativeIK(
-            lambda q: test_iterative_ik.robot.jacobian(
-                q, test_iterative_ik.wrist_index, 
-                update_kinematics=True, 
-                local_frame=False),
-            lambda q: test_iterative_ik.forward_kinematics(q),
+            None,
+            test_iterative_ik.jacobian,
+            test_iterative_ik.forward_kinematics,
             test_iterative_ik.active_dofs,
             test_iterative_ik.lower_limits,
             test_iterative_ik.upper_limits)
     iterative_ik.q_full = np.copy(test_iterative_ik.robot.q0)
+    iterative_ik.debug = False
 
     for i, config in enumerate(data):
         # q = config[0] 
@@ -60,20 +59,15 @@ if __name__== "__main__":
                     test_iterative_ik.wrist_index, 
                     update_kinematics=True, 
                     local_frame=False)
-        print "J_pin"
+        print "J local false"
         print J
 
-        print "J_lambda"
+        print "J ik"
         print iterative_ik.jacobian(q[:2])
         print iterative_ik.full_dof_config(q[:2]).transpose()
 
-        J = test_iterative_ik.robot.jacobian(
-                    iterative_ik.full_dof_config(
-                        q[test_iterative_ik.active_dofs]), 
-                    test_iterative_ik.wrist_index, 
-                    update_kinematics=True, 
-                    local_frame=False)
-        print "J_pin2"
+        J = test_iterative_ik.jacobian(q)
+        print "J local true + rot"
         print J
 
     print  "success : ", success