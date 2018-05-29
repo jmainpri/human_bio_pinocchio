42fdb790d9c3c0f70e83fc43f024e8631ec74f45
Jim Mainprice <jim.mainprice@ipvs.uni-stuttgart.de>
2018-05-29 13:56:15 +0200

    add tf skeleton script

---
+++ src/skeleton_tf.py
@@ -0,0 +1,42 @@
+import h5py
+import rospy
+import tf
+import optparse
+import os
+
+def publishTransforms(row, br):
+    """ all joint data is concatenated in one row """
+    for i in range(0, len(row), 7):
+        pos = row[i: i + 3]             # 3 fields position data
+        rot = row[i + 3: i + 7]         # 4 fields orientation data
+        br.sendTransform(pos, rot, rospy.Time.now(), "joint" + str(i), "base")
+
+
+def publish_hdf5_file(filename):
+    # setup rospy and broadcaster
+    rospy.init_node('skeleton_tf_node')
+    br = tf.TransformBroadcaster()
+
+    # read skeletondata from h5py
+    file = h5py.File(filename, 'r')
+    skeletonname = file['skeletons'].keys()[0]
+    skeletondata = file['skeletons'][skeletonname][:]  # open as numpy array
+
+    # iterate through file and publish transforms
+    rate = rospy.Rate(120)
+    for i, row in enumerate(skeletondata):
+        publishTransforms(row, br)
+        if i % 400 == 0:
+            print "progress : {} %".format(float(i) / float(len(skeletondata)))
+        rate.sleep()
+
+
+if __name__ == '__main__':
+
+    parser = optparse.OptionParser(
+        description='Pulbish hdf5 file to ROS TF')
+    parser.add_option('-f', 
+        default="mocap_data_1526395234.hdf5", type="str", dest='filename',
+        help='Path to the file')
+    (options, args) = parser.parse_args()
+    publish_hdf5_file(options.filename)
---
+++ src/visualize_tf_data.sh
@@ -0,0 +1,1 @@
+python skeleton_tf.py -f /workspace/workspace/mocap-mlr/interpolated/mocap_data_1517838370.hdf5
