fc0dc1b8f918baac25ad7dc88fb1c7a498e82ac3
Jim Mainprice <mainprice@gmail.com>
2018-06-05 19:30:57 +0200

    Fix bio motive ik

--- src/bio_motive_ik.py
+++ src/bio_motive_ik.py
@@ -22,7 +22,6 @@ from bioik import *
 import numpy.linalg as la
 from list_joint_names import *
 
-
 class BioMotiveIk():
 
     def __init__(self, semantics):
@@ -37,8 +36,12 @@ class BioMotiveIk():
 
     def joint_state(self, frame):
         t_pelvis    = self.semantics_.pelvis(frame)
+        t_pelvis    = Affine3d(
+            t_pelvis.matrix() *
+            MakeTransform(rodrigues([0, 0, -pi/2]), matrix([0, 0, 0])))
         t_p_inv     = la.inv(t_pelvis.matrix())
-        t_torso     = self.joint_frame(t_p_inv, frame, 'Torso')
+
+        # t_torso = self.joint_frame(t_p_inv, frame, 'Torso')
         r_shoulder  = self.joint_frame(t_p_inv, frame, 'rShoulder')
         r_elbow     = self.joint_frame(t_p_inv, frame, 'rElbow')
         r_wrist     = self.joint_frame(t_p_inv, frame, 'rWrist')
@@ -46,8 +49,21 @@ class BioMotiveIk():
         l_elbow     = self.joint_frame(t_p_inv, frame, 'lElbow')
         l_wrist     = self.joint_frame(t_p_inv, frame, 'lWrist')
 
-        pelvis_euler = euler_from_matrix(t_pelvis.linear(), 'rxzy')
+        t_torso = Affine3d(eye(4))
+        t_torso.translation += array([0.037432, 0, 0.139749])
 
+        pelvis_euler    = euler_from_matrix(t_pelvis.linear(), 'rxyz')
+        torso_euler     = array([0.]*3)
+        shoulder_euler  = euler_from_matrix(
+            t_offset * r_shoulder.linear(), 'ryxy')
+        shoulder_euler  = array([0.]*3) 
+        elbow_euler     = array([0.]*3) # euler_from_matrix(r_elbow.linear(), 'rxyz')
+        wrist_euler     = array([0.]*3) # euler_from_matrix(r_wrist.linear(), 'rxyz')
+
+        # matrix([[-1.0, 0.0, 0.0], [0.0, 0.0, 1], [0.0, 1.0, 0.0]])
+
+        # Expresses the shoulder center in
+        # in the torso frame.
         t_torso_inv = Affine3d(la.inv(t_torso.matrix()))
         shoulder_center = t_torso_inv * r_shoulder.translation
         
@@ -66,11 +82,23 @@ class BioMotiveIk():
         q["PelvisRotX"]         = pelvis_euler[0]
         q["PelvisRotY"]         = pelvis_euler[1]
         q["PelvisRotZ"]         = pelvis_euler[2]
-        q["rShoulderTransX"]    = shoulder_center[0]
-        q["rShoulderTransY"]    = shoulder_center[1]
-        q["rShoulderTransZ"]    = shoulder_center[2]
+        q["TorsoX"]             = torso_euler[0]
+        q["TorsoZ"]             = torso_euler[1]
+        q["TorsoY"]             = torso_euler[2]
+        q["rShoulderTransX"]    = shoulder_center[0]    # X
+        q["rShoulderTransY"]    = shoulder_center[2]    # Z
+        q["rShoulderTransZ"]    = -shoulder_center[1]   # -Y
         q["rArmTrans"]          = d_r_shoulder_to_elbow
         q["rForeArmTrans"]      = d_r_elbow_to_wrist
+        q["rShoulderY1"]        = shoulder_euler[0]
+        q["rShoulderX"]         = shoulder_euler[0]
+        q["rShoulderY2"]        = shoulder_euler[2]
+        q["rElbowZ"]            = elbow_euler[0]
+        q["rElbowX"]            = elbow_euler[1]
+        q["rElbowY"]            = elbow_euler[2]
+        q["rWristZ"]            = wrist_euler[0]
+        q["rWristX"]            = wrist_euler[1]
+        q["rWristY"]            = wrist_euler[2]
         return q
 
 
--- src/bioik/__init__.py
+++ src/bioik/__init__.py
@@ -1,1 +1,3 @@
 from math_utils import *
+from transform_matrix import *
+from rodrigues import *
